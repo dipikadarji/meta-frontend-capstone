9fc47bf26a1b380ed7d0e27eacae18d59f79c705
Dipika Darji <dipika.darji@aaravsolutions.com>
2026-03-20 18:03:51 +0530

    Confirme Booking

---
+++ little-lemon/src/components/ConfirmedBooking.js
@@ -0,0 +1,9 @@
+function ConfirmedBooking() {
+  return (
+    <div style={{ textAlign:"center", padding:"60px" }}>
+      <h2 style={{ color:"#495E57" }}>Booking Confirmed!</h2>
+      <p>Thank you! We look forward to seeing you at Little Lemon.</p>
+    </div>
+  );
+}
+export default ConfirmedBooking;
