5789527c6b43dcd74a22118bf81e31fa02b92ed0
Dipika Darji <dipika.darji@aaravsolutions.com>
2026-03-20 17:45:29 +0530

    Header & Nav File

---
+++ little-lemon/src/components/Footer.js
@@ -0,0 +1,14 @@
+{`// src/components/Footer.js
+function Footer() {
+  return (
+    
+
+      
+Little Lemon © 2024
+
+
+    
+
+  );
+}
+export default Footer;`}
---
+++ little-lemon/src/components/Nav.js
@@ -0,0 +1,17 @@
+{`// src/components/Nav.js
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    
+
+      Home
+      Reservations
+      Menu
+      About
+      Login
+    
+
+  );
+}
+export default Nav;`}
