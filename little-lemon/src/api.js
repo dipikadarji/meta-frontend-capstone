bcdda969a9927f6ea239f1165bbaf4f86a03d840
Dipika Darji <dipika.darji@aaravsolutions.com>
2026-03-20 17:43:27 +0530

    Folder & API file Creation

---
+++ little-lemon/src/api.js
@@ -0,0 +1,9 @@
+{`// src/api.js
+export function fetchAPI(date) {
+  const times = ["17:00","18:00","19:00","20:00","21:00","22:00"];
+  return times;
+}
+
+export function submitAPI(formData) {
+  return true;
+}`}
